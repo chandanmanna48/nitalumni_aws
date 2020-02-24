d8733db55aee4ffc722ac60eabb28ce9762d2c2e
chandanmanna48 <chandanmanna48@gmail.com>
2020-02-24 16:43:27 +0530

    commits

--- myalumini/settings.py
+++ myalumini/settings.py
@@ -163,10 +163,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+#STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
