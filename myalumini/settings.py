3789d572fed3a056ba929ce812788106d6f6325b
chandanmanna48 <chandanmanna48@gmail.com>
2020-03-03 00:27:57 +0530

    commits

--- myalumini/settings.py
+++ myalumini/settings.py
@@ -254,6 +254,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_DEFAULT_ACL = None
+#AWS_S3_FILE_OVERWRITE = False
 AWS_LOCATION = 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
@@ -261,4 +262,4 @@ STATICFILES_DIRS = [
 STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+DEFAULT_FILE_STORAGE = 'myalumini.storage_backends.MediaStorage'
