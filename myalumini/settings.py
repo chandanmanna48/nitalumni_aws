bd051c9cbbdb78c460ebc07470ee6facaf785ac4
chandanmanna48 <chandanmanna48@gmail.com>
2020-03-02 21:30:43 +0530

    commits

--- myalumini/settings.py
+++ myalumini/settings.py
@@ -249,13 +249,15 @@ EMAIL_HOST_PASSWORD='nalanda@2020'
 AWS_ACCESS_KEY_ID = 'AKIAT33ESB4CLXT5SPBK'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'alumninit-storage'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
-
-
 
 
